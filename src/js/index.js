c6258e85ccb5fa765ac7ad995f99a8e7d367fe44
Nerz <115172452+Nerzzz@users.noreply.github.com>
2026-01-26 15:40:11 -0300

    API's time_stamp url missing fixed

--- src/js/index.js
+++ src/js/index.js
@@ -22,7 +22,7 @@ function formatTemperature(temperature, unit){
 
 async function getDaily(lat, lon) {
     try{        
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=precipitation_probability_max,weather_code&hourly=temperature_2m,precipitation,weather_code&current=temperature_2m,apparent_temperature,is_day,weather_code,precipitation,relative_humidity_2m&forecast_days=1`, {
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timezone=auto&daily=precipitation_probability_max,weather_code&hourly=temperature_2m,precipitation,weather_code&current=temperature_2m,apparent_temperature,is_day,weather_code,precipitation,relative_humidity_2m&forecast_days=1`, {
             method: "GET"
         }).then((_response) => {
             return _response.json();
@@ -31,6 +31,8 @@ async function getDaily(lat, lon) {
             const current = data.current;
             const units = data.current_units;
             const hourUnits = data.hourly_units
+
+            console.log(data);
 
             set("current-temperature", formatTemperature(current.temperature_2m, units.temperature_2m));
 
@@ -44,12 +46,14 @@ async function getDaily(lat, lon) {
             data.hourly.time.forEach((hour, index) => {
                 const clone = templateItem.content.cloneNode(true);
 
+                clone.querySelector(".hourly-item").setAttribute("id", `${new Date(hour).getHours()}`);
                 clone.querySelector(".hour").innerHTML = formatHour(hour);
                 clone.querySelector(".temperature").innerHTML = formatTemperature(data.hourly.temperature_2m[index], hourUnits.temperature_2m);
-                clone.querySelector(".probability").innerHTML = `${data.hourly.precipitation[index]}${hourUnits.precipitation}`;
+                clone.querySelector(".probability").innerHTML = `${Math.round(data.hourly.precipitation[index])}${hourUnits.precipitation}`;
 
                 hourlyContainer.appendChild(clone);
             });
+            document.getElementById(`${new Date(data.current.time).getHours()}`).scrollIntoView();
 
         });
     }
