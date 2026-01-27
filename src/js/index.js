54f23891c760258acd96399730cf57ceb170d35c
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-01-27 15:36:27 -0300

    UI updates

--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,23 @@ function formatTemperature(temperature, unit){
     return `${Math.round(temperature)}${unit}`;
 }
 
+function selectWeatherIlustrationSrc(wmo, hour){
+    const folder = (hour > 5 && hour < 19) ? "day" : "night";
+
+    let weather;
+    if(wmo == 0){ weather = "clear"; }
+    else if(wmo == 1){ weather = "mostly-clear"; }
+    else if(wmo == 2){ weather = "partly-cloudy"; }
+    else if(wmo >= 3 && wmo <= 48){ weather = "mostly-cloudy"; }
+    else if(wmo >= 51 && wmo <= 55){ weather = "light-rain"; }
+    else if(wmo >= 61 && wmo <= 65){ weather = "heavy-rain"; }
+    else if(wmo >= 80 && wmo <= 82){ weather = "light-rain"; }
+    else if(wmo >= 71 && wmo <= 77){ weather = "snow"; }
+    else if(wmo >= 95){ weather = "snow"; }
+    else { weather = "clear"; }
+
+    return `./assets/images/climate/${folder}/${weather}.svg`;
+}
 async function getDaily(lat, lon) {
     try{        
         fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timezone=auto&daily=precipitation_probability_max,weather_code&hourly=temperature_2m,precipitation,weather_code&current=temperature_2m,apparent_temperature,is_day,weather_code,precipitation,relative_humidity_2m&forecast_days=1`, {
@@ -31,7 +48,15 @@ async function getDaily(lat, lon) {
             const units = data.current_units;
             const hourUnits = data.hourly_units
 
-            console.log(data);
+            const hour = new Date(current.time).getHours();
+            if(hour > 5 && hour < 19) {
+                document.querySelector(".current-weather").classList.add("day");
+                document.querySelector(".current-weather").classList.remove("night");
+            }
+            else {
+                document.querySelector(".current-weather").classList.remove("day");
+                document.querySelector(".current-weather").classList.add("night");
+            }
 
             set("current-temperature", formatTemperature(current.temperature_2m, units.temperature_2m));
 
@@ -39,20 +64,30 @@ async function getDaily(lat, lon) {
 
             set("current-prec-rate", `${data.daily.precipitation_probability_max}${data.daily_units.precipitation_probability_max}`);
 
+            document.getElementById("current-ilustration").setAttribute("src", selectWeatherIlustrationSrc(current.weather_code, hour));
+
             const hourlyContainer = document.getElementById("hourly-weather");
             const templateItem = document.getElementById("hourly-item-template");
 
-            data.hourly.time.forEach((hour, index) => {
+            data.hourly.time.forEach((_hour, index) => {
                 const clone = templateItem.content.cloneNode(true);
 
-                clone.querySelector(".hourly-item").setAttribute("id", `${new Date(hour).getHours()}`);
-                clone.querySelector(".hour").innerHTML = formatHour(hour);
+                if(hour > new Date(_hour).getHours()){
+                    clone.querySelector(".hourly-item").classList.add("past");
+                }
+
+                clone.querySelector(".hourly-item").setAttribute("id", `${new Date(_hour).getHours()}`);
+                
+                clone.querySelector(".ilustration").setAttribute("src", selectWeatherIlustrationSrc(data.hourly.weather_code[index], `${new Date(_hour).getHours()}`))
+                clone.querySelector(".hour").innerHTML = formatHour(_hour);
                 clone.querySelector(".temperature").innerHTML = formatTemperature(data.hourly.temperature_2m[index], hourUnits.temperature_2m);
                 clone.querySelector(".probability").innerHTML = `${Math.round(data.hourly.precipitation[index])}${hourUnits.precipitation}`;
 
                 hourlyContainer.appendChild(clone);
             });
-            document.getElementById(`${new Date(data.current.time).getHours()}`).scrollIntoView();
+
+            document.getElementById(hour).scrollIntoView();
+            document.getElementById(hour).classList.add("now");
 
         });
     }
